9e2b7129ecc63e0bff40d4924fc6f1673407ee1d
NiklasMelton <niklasmelton@gmail.com>
2019-08-09 00:50:59 -0500

    Update reset.py

--- libs/reset_device/reset.py
+++ libs/reset_device/reset.py
@@ -8,13 +8,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 counter = 0
-serial_last_four = subprocess.check_output(['cat', '/proc/cpuinfo'])[-5:-1].decode('utf-8')
-config_hash = reset_lib.config_file_hash()
-ssid_prefix = config_hash['ssid_prefix'] + " "
-reboot_required = False
+# serial_last_four = subprocess.check_output(['cat', '/proc/cpuinfo'])[-5:-1].decode('utf-8')
+# config_hash = reset_lib.config_file_hash()
+# ssid_prefix = config_hash['ssid_prefix'] + " "
+# reboot_required = False
 
 
-reboot_required = reset_lib.wpa_check_activate(config_hash['wpa_enabled'], config_hash['wpa_key'])
+# reboot_required = reset_lib.wpa_check_activate(config_hash['wpa_enabled'], config_hash['wpa_key'])
 
 #reboot_required = reset_lib.update_ssid(ssid_prefix, serial_last_four)
 
