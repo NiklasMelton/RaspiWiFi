561f38d49821bee061c50aee9c9b71cb494d2f71
NiklasMelton <niklasmelton@gmail.com>
2019-08-09 00:51:26 -0500

    Update reset.py

--- libs/reset_device/reset.py
+++ libs/reset_device/reset.py
@@ -11,7 +11,7 @@ counter = 0
 # serial_last_four = subprocess.check_output(['cat', '/proc/cpuinfo'])[-5:-1].decode('utf-8')
 # config_hash = reset_lib.config_file_hash()
 # ssid_prefix = config_hash['ssid_prefix'] + " "
-# reboot_required = False
+reboot_required = False
 
 
 # reboot_required = reset_lib.wpa_check_activate(config_hash['wpa_enabled'], config_hash['wpa_key'])
